bd686f6e27686b88545184f0b10990e9127849af
Aymen <67931880+Aymen-bk@users.noreply.github.com>
2025-10-18 18:03:28 +0100

    Update IntegerList.java

--- challenge/src/main/java/problem2/IntegerList.java
+++ challenge/src/main/java/problem2/IntegerList.java
@@ -3,12 +3,16 @@ package problem2;
 public class IntegerList
 {
     int[] list; //values in the list
+    int cIntegers;
+    int cSize;
     //-------------------------------------------------------
 //create a list of the given size
 //-------------------------------------------------------
     public IntegerList(int size)
     {
         list = new int[size];
+        cIntegers = size;
+        cSize = size;
     }
     //-------------------------------------------------------
 //fill array with integers between 1 and 100, inclusive
@@ -17,6 +21,7 @@ public class IntegerList
     {
         for (int i=0; i<list.length; i++)
             list[i] = (int)(Math.random() * 100) + 1;
+        cIntegers = cSize;
     }
     //-------------------------------------------------------
 //print array elements with indices
@@ -25,5 +30,43 @@ public class IntegerList
     {
         for (int i=0; i<list.length; i++)
             System.out.println(i + ":\t" + list[i]);
+    }
+
+    public void increaseSize() {
+        cSize *= 2;
+        int[] newList = new int[cSize];
+        System.arraycopy(list, 0, newList, 0, cIntegers);
+        list = newList; }
+
+    void addElement(int newVal){
+        if (cIntegers==cSize)
+        {
+            increaseSize();
+        }
+        list[cIntegers++] = newVal;
+    }
+
+    public void removeFirst(int newVal){
+        for(int i=0; i<cIntegers; i++)
+        {
+            if(list[i] == newVal)
+            {
+                for (int j=i;j<cIntegers-1;j++) { list[j] = list[j+1]; } //we shift and erase the L[i]
+            cIntegers--;
+            }
+        }
+    }
+
+    public void removeALL(int newVal){
+        int index=0;
+        for(int i=0; i<list.length; i++)
+        {
+            if(list[i]==newVal)
+            {
+                index=i;
+                cIntegers--;
+                for(int j=index; j<cIntegers; j++){ list[j] = list[j+1]; } //shifting
+            }
+        }
     }
 }
