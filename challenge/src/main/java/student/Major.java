dc4f8e5b1333942c5a39b627365d50fd502f4b62
Aymen <67931880+Aymen-bk@users.noreply.github.com>
2025-10-18 19:32:27 +0100

    Enhance Major class with student management features

--- challenge/src/main/java/student/Major.java
+++ challenge/src/main/java/student/Major.java
@@ -6,24 +6,66 @@ public class Major {
     private String code;
     private String name;
     private Student[] students;
+
     private int studentCount;
+    private static final int max = 50;
 
     public Major(String code, String name) {
-
+        this.id = nextId++;
+        this.code = code;
+        this.name=name;
+        this.students=new Student[max];
+        this.studentCount =0;
     }
 
-    // Method to add a student
+    public Major() {
+        this("23", "Computer Science"); //default
+    }
+
     public void addStudent(Student s) {
+        if (studentCount < max) { students[studentCount++] = s; }
 
+        else {System.out.println("Cannot add more students, capacity full for " + name);}
     }
 
-    // Getters
+    public Student findStudentByCNE(String cne) {
+        for (int i = 0; i < studentCount; i++)
+        {
+            if (students[i].getCne().equals(cne)) {return students[i];}
+        }
+        return null; //not found
+    }
 
+    public boolean removeStudent(String cne) {
+        for (int i = 0; i < studentCount; i++) { //very similar shifting method  in problem2
+            if (students[i].getCne().equals(cne))
+            {
+                for (int j = i; j < studentCount - 1; j++) {students[j] = students[j + 1];}
+                students[--studentCount] = null;
+                return true;
+            }
+        }
+        return false;
+    }
 
-    // Display all students in the major
+    //some getters
+    public int getStudentCount() { return studentCount;}
+    public double getOccupancyRate() {return (studentCount * 100.0) / max;}
+
+    public String getStudentListAsString() {
+        StringBuilder sb = new StringBuilder(); //using string builder
+        for (int i = 0; i < studentCount; i++) {
+            sb.append(i + 1).append(". ").append(students[i].toString()).append("\n");
+        }
+        return sb.toString();
+    }
+
     public void displayStudents() {
-
+        System.out.println("Major " + name + " has :");
+        System.out.println(getStudentListAsString());
     }
 
-
+    public String toString() {
+        return String.format("Major[%s - %s] (%d/%d students)", code, name, studentCount, max);
+    }
 }
